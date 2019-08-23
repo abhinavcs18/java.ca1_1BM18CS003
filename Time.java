72c397b3ddda5105db831d2e841c2476673bf625
abhinavcs18 <53806523+abhinavcs18@users.noreply.github.com>
2019-08-23 22:00:41 +0530

    Update Time.java

--- Time.java
+++ Time.java
@@ -27,36 +27,38 @@ class Time
 	}
 	void print()
 	{
-		System.out.println("Hour = "+n);
+		System.out.println("Hour = "+h);
 		System.out.println("Minute = "+m);
 		System.out.println("Second = "+s);
 	}
+}
 	class TimeDemo
 	{
-		public static void main(strings args[])
+		public static void main(String args[])
 		{
 			Scanner sc = new Scanner(System.in);
 		while(true)
 		{
 			System.out.print("Enter your choice");
-			ch = sc.nextInt();
-			switch(ch)
+		    int	ch = sc.nextInt();
+	        switch(ch)
 			{
 				case 1:
 					Time a = new Time();
 					System.out.println("Enter hour,minute and second");
 					int hh = sc.nextInt();
 					int mm = sc.nextInt();
-					INT ss = sc.nextInt();
+					int ss = sc.nextInt();
 					a.currentTime(hh,mm,ss);
 					a.print();
 					break;
 				case 2:
-					Time ob = new Time(hh,mm,ss);
+			        Time ob = new Time(hh,mm,ss);
 					break;
+			}
+		}
+		}
 	}
-}
-
 
 
 
